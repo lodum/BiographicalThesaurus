43369b42f682bed654ee1c08f2b87f576b9400b6
Jakob Moellers <jakob.moellers@gmail.com>
2012-12-07 19:42:29 +0100

    website multilingual support implemented for stis.php

---
+++ website_mockups/jakob/stis/docs/examples/languages/lang.de.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+-----------------
+Language: German
+-----------------
+*/
+
+$lang = array();
+
+//stis.php
+
+$lang['STIS_PAGE_TITLE'] = 'Biographischer Thesaurus NRW';
+$lang['STIS_HERO_HEADER'] = 'Willkommen';
+$lang['STIS_HERO_TEXT'] = 'Dies ist der biographische Thesaurus NRW. Hier finden Sie Informationen über Personen, Autoren und Veröffentlichungen. Erkunden Sie die biographische Suche nach Personen, die in einer Verbindung zu Nordrhein-Westfalen (NRW) stehen.';
+$lang['STIS_HERO_BUTTON'] = 'Suche Starten &raquo;';
+
+?>
---
+++ website_mockups/jakob/stis/docs/examples/languages/lang.en.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+//stis.php
+
+$lang['STIS_PAGE_TITLE'] = 'Biographical Thesaurus NRW';
+$lang['STIS_HERO_HEADER'] = 'Welcome';
+$lang['STIS_HERO_TEXT'] = 'This is the Biographical Thesaurus NRW. Here, you will find information on authors and publications. Explore the biographical search application about people with reference to North Rhine-Westphalia (NRW)';
+$lang['STIS_HERO_BUTTON'] = 'Start searching! &raquo;';
+
+
+
+?>
