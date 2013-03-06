d599281a70589719c24b1ded35ad37aed4e797f9
Jakob Moellers <jakob.moellers@gmail.com>
2013-03-06 14:57:38 +0100

    new index without duplicate. creation of lucene index fails but duplicates are removed sucessfully.

---
+++ website_mockups/matthias/stis/test_easyrdf2.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Making a SPARQL SELECT query
+ *
+ * This example creates a new SPARQL client, pointing at the
+ * dbpedia.org endpoint. It then makes a SELECT query that
+ * returns all of the countries in DBpedia along with an
+ * english label.
+ *
+ * Note how the namespace prefix declarations are automatically
+ * added to the query.
+ *
+ * @package    EasyRdf
+ * @copyright  Copyright (c) 2009-2012 Nicholas J Humfrey
+ * @license    http://unlicense.org/
+ */
+#echo get_include_path();
+set_include_path('./vendor/easyrdf/easyrdf/examples/');
+require_once "html_tag_helpers.php";
+set_include_path('./vendor/easyrdf/easyrdf/lib/');
+require 'vendor/autoload.php';
+require_once "EasyRdf.php";
+#require_once "html_tag_helpers.php";
+include_once '/var/www/php/Zend/Loader.php';
+Zend_Loader::registerAutoload();
+
+//Get the triples
+
+EasyRdf_Namespace::set('stis', 'http://localhost/default#');
+EasyRdf_Namespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+EasyRdf_Namespace::set('gnd', 'http://d-nb.info/standards/elementset/gnd#');
+$sparql = new EasyRdf_Sparql_Client('http://data.uni-muenster.de/bt/sparql');
+$result = $sparql->query('SELECT * WHERE {?a gnd:placeOfDeath ?deathEntity. ?deathEntity gnd:preferredName ?name.} LIMIT 10');
+
+//Filter results for duplicates
+//[][0] name; [][1]uri
+
+echo "raw results...\n";
+$rawResult = array(array(2));
+$i=0;
+foreach ($result as $row){
+	$rawResult[$i][0]=$row->name;
+	$rawResult[$i][1]=$row->deathEntity;
+	$i++;
+}
+
+for ($i=0;$i<sizeof($rawResult);$i++){
+	echo $rawResult[$i][0];
+	echo " " ;
+	echo $rawResult[$i][1];
+	echo "\n";
+}
+
+echo "filtering... \n";
+
+$filteredResult = array();
+
+//echo $filteredResult[0];
+//echo "\n";
+
+for($i=0;$i<sizeof($rawResult);$i++) {
+	$name=$rawResult[$i][0];
+	echo $name;
+	echo "\n";
+	$uri=$rawResult[$i][1];
+	$filteredResult[''.$name.'']=$uri;
+	
+	
+	//$found = false;
+	
+	/*for ($j=0;$j<sizeOf($filteredResult);$j++){
+		echo $rawResult[$i][1];
+		echo " ";
+		echo $filteredResult[$j][1];
+		echo "\n";
+	}*/
+	
+	/*for ($j=0;$j<sizeof($filteredResult);$j++){
+		//$filteredResult[i] = new Array();
+		echo $rawResult[$i][1];
+		echo $filteredResult[$j][1];
+		if ($rawResult[$i][1]==$filteredResult[$j][1]){
+			$found=true;
+			echo "found one ";
+			echo $rawResult[$i][1];
+			echo " ";
+			echo $filteredResult[$j][1];
+			echo "\n";
+		}
+	}*/
+	
+	//echo "size: ";
+	//echo sizeof($filteredResult);
+	//echo "\n";
+	
+	/*if ($found==false){
+		$filteredResult[sizeof($filteredResult)][0]=$rawResult[$i][0];
+		$filteredResult[sizeof($filteredResult)][1]=$rawResult[$i][1];
+	}*/
+}
+
+echo "filtered results \n";
+
+foreach ($filteredResult as $name => $uri){
+	echo $name;
+	echo " ";
+	echo $uri;
+	echo "\n";
+}
+
+/*for ($i=0;$i<sizeof($filteredResult);$i++){
+	echo $filteredResult[$i][0];
+	echo " " ;
+	echo $filteredResult[$i][1];
+	echo "\n";
+}*/
+
+echo "create index \n";
+
+//Create placesOfDeath Index
+$index = Zend_Search_Lucene::create('./index_placesOfDeath');
+foreach ($filteredResult as $name => $uri){
+    #echo "<li>".link_to($row->label, $row->country)."</li>\n";
+    #echo $row->a." ".$row->name."\n";
+    $document = new Zend_Search_Lucene_Document();
+    $document->addField(Zend_Search_Lucene_Field::UnIndexed('URI', $uri));
+    $document->addField(Zend_Search_Lucene_Field::Text('placeOfDeath', $name));
+    $index->addDocument($document);
+    echo "add ";
+    echo $uri;
+    echo " ";
+    echo $name;
+    echo "\n";
+}
+
+//Echo placesOfDeath Index into json file
+
+$index = Zend_Search_Lucene::open('./index_placesOfDeath');
+$queries = array(
+    '*'
+);
+
+foreach ($queries as $query) {
+	Zend_Search_Lucene_Search_Query_Wildcard::setMinPrefixLength(0);
+    $results = $index->find(Zend_Search_Lucene_Search_QueryParser::parse($query));
+    echo "Suche: " . $query . "\n";
+    echo count($results) . " Ergebnisse \n\n";
+    $posts=array();
+    foreach ($results as $result) {
+        echo 'URI: ' . $result->URI . "\n";
+		$uri=$result->URI;
+        echo 'Place Of Death: ' . $result->placeOfDeath . "\n";
+        $placeOfDeath=$result->placeOfDeath;
+        #echo 'Score: ' . $result->score . "\n";
+        echo "\n";
+        $posts[]=array('uri'=>$uri,'placeOfDeath'=>$placeOfDeath);
+    }
+    $response=array();
+    $response['placesOfDeath']=$posts;
+    $fp = fopen('placesOfDeath.json', 'w');
+	fwrite($fp, json_encode($response));
+	fclose($fp);
+    
+}
+?>
