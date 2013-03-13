b3f4b3a9e6b8684dd25bf55c75395a762876c725
Jakob Moellers <jakob.moellers@gmail.com>
2013-03-13 16:01:42 +0100

    person search with index included.
    issue: because of the structure of the submitCustomQuery function, only one person can be searched at the same time. This issue has to be coped with if one for example searches for 'dieter', he only gets one result although the search index returns multiple dieters...

--- website_mockups/matthias/stis/buildPersonIndex.php
+++ website_mockups/matthias/stis/buildPersonIndex.php
@@ -23,6 +23,8 @@ require_once "EasyRdf.php";
 #require_once "html_tag_helpers.php";
 include_once '/var/www/php/Zend/Loader.php';
 Zend_Loader::registerAutoload();
+//TODO set this to infinity
+//Zend_Search_Lucene::setTermsPerQueryLimit(1000000);
 
 //Get the triples
 
@@ -31,23 +33,16 @@ EasyRdf_Namespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
 EasyRdf_Namespace::set('gnd', 'http://d-nb.info/standards/elementset/gnd#');
 $sparql = new EasyRdf_Sparql_Client('http://data.uni-muenster.de/bt/sparql');
 //$result1 = $sparql->query('SELECT * WHERE {?a gnd:placeOfDeath ?deathEntity. ?deathEntity gnd:preferredName ?name.} OFFSET 10000 LIMIT 20000');
-//$result2 = $sparql->query('SELECT * WHERE {?a gnd:placeOfBirth ?birthEntity. ?birthEntity gnd:preferredName ?name.} OFFSET 10000 LIMIT 20000');
-$result1 = $sparql->query('SELECT * WHERE {?a gnd:placeOfDeath ?deathEntity. ?deathEntity gnd:preferredName ?name.} LIMIT 20000');
-$result2 = $sparql->query('SELECT * WHERE {?a gnd:placeOfBirth ?birthEntity. ?birthEntity gnd:preferredName ?name.} LIMIT 20000');
+$result = $sparql->query('select * where{ ?uri a stis:Person. ?uri gnd:preferredNameForThePerson ?name .} LIMIT 1000');
 
 //Filter results for duplicates
 //[][0] name; [][1]uri
 
 $rawResult = array(array(2));
 $i=0;
-foreach ($result1 as $row){
+foreach ($result as $row){
 	$rawResult[$i][0]=$row->name;
-	$rawResult[$i][1]=$row->deathEntity;
-	$i++;
-}
-foreach ($result2 as $row){
-	$rawResult[$i][0]=$row->name;
-	$rawResult[$i][1]=$row->birthEntity;
+	$rawResult[$i][1]=$row->uri;
 	$i++;
 }
 
@@ -60,19 +55,19 @@ for($i=0;$i<sizeof($rawResult);$i++) {
 }
 
 //Create placesOfDeath Index
-$index = Zend_Search_Lucene::create('./index_places');
+$index = Zend_Search_Lucene::create('./index_persons');
 foreach ($filteredResult as $name => $uri){
     #echo "<li>".link_to($row->label, $row->country)."</li>\n";
     #echo $row->a." ".$row->name."\n";
     $document = new Zend_Search_Lucene_Document();
     $document->addField(Zend_Search_Lucene_Field::UnIndexed('URI', $uri));
-    $document->addField(Zend_Search_Lucene_Field::Text('place', $name));
+    $document->addField(Zend_Search_Lucene_Field::Text('person', $name));
     $index->addDocument($document);
 }
 
 $index->commit();
 
-$index = Zend_Search_Lucene::open('./index_places');
+$index = Zend_Search_Lucene::open('./index_persons');
 $queries = array(
     '*'
 );
@@ -84,17 +79,17 @@ foreach ($queries as $query) {
     echo count($results) . " Ergebnisse \n\n";
     $posts=array();
     foreach ($results as $result) {
-        echo 'URI: ' . $result->URI . "\n";
+        //echo 'URI: ' . $result->URI . "\n";
 		$uri=$result->URI;
-        echo 'Place: ' . $result->place . "\n";
-        $place=$result->place;
+        //echo 'Person: ' . $result->person . "\n";
+        $person=$result->person;
         #echo 'Score: ' . $result->score . "\n";
-        echo "\n";
-        $posts[]=array('uri'=>$uri,'place'=>$place);
+        //echo "\n";
+        $posts[]=array('uri'=>$uri,'person'=>$person);
     }
     $response=array();
-    $response['place']=$posts;
-    $fp = fopen('places.json', 'w');
+    $response['person']=$posts;
+    $fp = fopen('person.json', 'w');
 	fwrite($fp, json_encode($response));
 	fclose($fp);
     
