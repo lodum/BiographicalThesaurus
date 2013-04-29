e1bff8e8fb8c87b00c5b37d94f19e032592283fe
Jakob Moellers <jakob.moellers@gmail.com>
2013-04-29 14:51:47 +0200

    documented extractorScript

--- Concept Group/Lobid-Extractor/extractorScript.sh
+++ Concept Group/Lobid-Extractor/extractorScript.sh
@@ -1,11 +1,15 @@
+echo StartExtractingLinesFromNTFiles
 for file in *.nt; do
 	#echo "$file"
 	java LineExtractor "$file"
 	cat out.txt >> results.txt
 done
 mv results.txt out.txt
+echo SplitOutTXTIntoDifferentFiles
 split -l 5000 out.txt out_split
+echo DownloadResourcesFromGND
 for file in out_split* do
 	echo "$file"
 	python dl-gnd.py "$file"
 done
+echo UploadResourcesToTripleStore
