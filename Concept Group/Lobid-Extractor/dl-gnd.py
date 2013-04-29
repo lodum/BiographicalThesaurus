e6062c74c7a403707a5ddbaf3aa2114d9075e1e7
Jakob Moellers <jakob.moellers@gmail.com>
2013-04-29 14:48:40 +0200

    revised scripts for automatic download from gnd

--- Concept Group/Lobid-Extractor/dl-gnd.py
+++ Concept Group/Lobid-Extractor/dl-gnd.py
@@ -2,27 +2,28 @@
 
 import urllib2
 import os.path
+import sys
 
-
-f = open('out.txt','r')
+#f = open('out.txt','r')
+f=sys.argv[1]
 errors = open('errors.txt','w')
 
 for line in f.readlines():
-        filename = line.split('/')[-3]
-        filename = filename+('.rdf');
-        print filename
-        if not (os.path.isfile('out/'+filename[:-1])):
-                try:
-                        r = urllib2.urlopen(line)
-                        rdf = r.read()
-                        #out_file = open('out/'+filename[:-1],'w')
-                        out_file = open('out/'+filename,'w')
-                        out_file.write(rdf)
-                        out_file.close()
-                        #print 'done loading '+filename[:-1]
-                except urllib2.HTTPError, e:                        
-                        errors.write(line)
-                        print 'error '+e
-                
-        #else:
-                #print 'exists'
+		filename = line.split('/')[-3]
+		filename = filename+('.rdf');
+		print filename
+		if not (os.path.isfile('out/'+filename[:-1])):
+				try:
+						r = urllib2.urlopen(line)
+						rdf = r.read()
+						#out_file = open('out/'+filename[:-1],'w')
+						out_file = open('out/'+filename,'w')
+						out_file.write(rdf)
+						out_file.close()
+						#print 'done loading '+filename[:-1]
+				except urllib2.HTTPError, e:                        
+						errors.write(line)
+						print 'error '+e
+				
+		#else:
+				#print 'exists'
--- Concept Group/Lobid-Extractor/extractorScript.sh
+++ Concept Group/Lobid-Extractor/extractorScript.sh
@@ -4,3 +4,8 @@ for file in *.nt; do
 	cat out.txt >> results.txt
 done
 mv results.txt out.txt
+split -l 5000 out.txt out_split
+for file in out_split* do
+	echo "$file"
+	python dl-gnd.py "$file"
+done
